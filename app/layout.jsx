b7ed7668787513d313198dee9774f92b711131c8
Ekansh-2004 <chauhan.ekansh06@gmail.com>
2025-07-09 18:51:35 +0530

    Intial Commit

---
+++ app/layout.jsx
@@ -0,0 +1,17 @@
+import "../assets/styles/global.css";
+
+export const metadata = {
+	title: "Property Rental",
+};
+
+const MainLayout = () => {
+	return (
+		<html>
+			<body>
+				<main className="text-7xl">Hello</main>
+			</body>
+		</html>
+	);
+};
+
+export default MainLayout;
